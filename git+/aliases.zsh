65a8bf758d3a63b6c02d3a318c9def599e027501
Tristan Hume <tris.hume@gmail.com>
2014-10-01 15:06:50 -0400

    Tuning

--- git+/aliases.zsh
+++ git+/aliases.zsh
@@ -25,7 +25,7 @@ alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+# alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 
 function mkcd
