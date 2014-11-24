822ca160d87aed0a6a5e51014c8dca382f352e68
Tristan Hume <tris.hume@gmail.com>
2014-11-24 17:27:02 -0500

    Better Mjolnir

--- mjolnir/mjolnir.symlink/init.lua
+++ mjolnir/mjolnir.symlink/init.lua
@@ -5,26 +5,23 @@ local hotkey = require "mjolnir.hotkey"
 local keycodes = require "mjolnir.keycodes"
 local fnutils = require "mjolnir.fnutils"
 local alert = require "mjolnir.alert"
-local grid = require "mjolnir.bg.grid"
 local screen = require "mjolnir.screen"
--- Music controls
+-- User packages
+local grid = require "mjolnir.bg.grid"
 local spotify = require "mjolnir.lb.spotify"
+local hints = require "mjolnir.th.hints"
+local appfinder = require "mjolnir.cmsj.appfinder"
 
--- Set up hotkey combinations
-local hyper = {"cmd", "alt", "ctrl","shift"}
--- Set grid size.
-grid.GRIDWIDTH  = 6
-grid.GRIDHEIGHT = 4
-grid.MARGINX = 0
-grid.MARGINY = 0
+local definitions = nil
+local hyper = nil
 
-local gridset = function(x, y, w, h)
+local gridset = function(frame)
 	return function()
 		local win = window.focusedwindow()
 		if win then
-			grid.set(win, {x=x, y=y, w=w, h=h}, win:screen())
+			grid.set(win, frame, win:screen())
 		else
-			alert.show("No focused window. Check your OS X Accessibility settings. Uncheck and check Mjolnir there if you have moved it.")
+			alert.show("No focused window.")
 		end
 	end
 end
@@ -40,17 +37,103 @@ function focusSaved()
   end
 end
 
-local definitions = {
+local hotkeys = {}
+
+function createHotkeys()
+  for key, fun in pairs(definitions) do
+    local mod = hyper
+    if string.len(key) == 2 and string.sub(key,2,2) == "c" then
+      mod = {"cmd"}
+    end
+
+    local hk = hotkey.new(mod, string.sub(key,1,1), fun)
+    table.insert(hotkeys, hk)
+    hk:enable()
+  end
+end
+
+function rebindHotkeys()
+  for i, hk in ipairs(hotkeys) do
+    hk:disable()
+  end
+  hotkeys = {}
+  createHotkeys()
+  alert.show("Rebound Hotkeys")
+end
+
+function applyPlace(win, place)
+  local scrs = screen:allscreens()
+  local scr = scrs[place[1]]
+  grid.set(win, place[2], scr)
+end
+
+function applyLayout(layout)
+  return function()
+    for appName, place in pairs(layout) do
+      local app = appfinder.app_from_name(appName)
+      if app then
+        for i, win in ipairs(app:allwindows()) do
+          applyPlace(win, place)
+        end
+      end
+    end
+  end
+end
+
+function init()
+  createHotkeys()
+  keycodes.inputsourcechanged(rebindHotkeys)
+  alert.show("Mjolnir, at your service.")
+end
+
+-- Actual config =================================
+
+hyper = {"cmd", "alt", "ctrl","shift"}
+-- Set grid size.
+grid.GRIDWIDTH  = 6
+grid.GRIDHEIGHT = 8
+grid.MARGINX = 0
+grid.MARGINY = 0
+local gw = grid.GRIDWIDTH
+local gh = grid.GRIDHEIGHT
+
+local gomiddle = {x = 1, y = 1, w = 4, h = 6}
+local goleft = {x = 0, y = 0, w = gw/2, h = gh}
+local goright = {x = gw/2, y = 0, w = gw/2, h = gh}
+local gobig = {x = 0, y = 0, w = gw, h = gh}
+
+local fullApps = {
+  "Safari","Aurora","Nightly","Xcode","Qt Creator","Google Chrome",
+  "Google Chrome Canary", "Eclipse", "Coda 2", "iTunes", "Emacs"
+}
+local layout2 = {
+  Airmail = {1, gomiddle},
+  Spotify = {1, gomiddle},
+  Calendar = {1, gomiddle},
+  Dash = {1, gomiddle},
+  iTerm = {2, goright},
+  MacRanger = {2, goleft},
+}
+fnutils.each(fullApps, function(app) layout2[app] = {1, gobig} end)
+
+definitions = {
   [";"] = saveFocus,
   a = focusSaved,
 
-  h = gridset(2, 1, 2, 2),
-  t = gridset(0, 0, 3, 4),
+  h = gridset(gomiddle),
+  t = gridset(goleft),
   n = grid.maximize_window,
-  s = gridset(3, 0, 3, 4),
+  s = gridset(goright),
+
+  g = applyLayout(layout2),
 
   d = grid.pushwindow_nextscreen,
-  r = mjolnir.reload
+  r = mjolnir.reload,
+  q = function() appfinder.app_from_name("Mjolnir"):kill() end,
+
+  k = function() hints.appHints(appfinder.app_from_name("Emacs")) end,
+  j = function() hints.appHints(window.focusedwindow():application()) end,
+  ec = hints.windowHints
 }
 
 -- launch and focus applications
@@ -64,26 +147,4 @@ fnutils.each({
     definitions[object.key] = function() application.launchorfocus(object.app) end
 end)
 
--- Bind the hotkey definitions
-local hotkeys = {}
-
-function createHotkeys()
-  for key, fun in pairs(definitions) do
-    local hk = hotkey.new(hyper, key, fun)
-    table.insert(hotkeys, hk)
-    hk:enable()
-  end
-end
-function rebindHotkeys()
-  for i, hk in ipairs(hotkeys) do
-    hk:disable()
-  end
-  hotkeys = {}
-  createHotkeys()
-  alert.show("Rebound Hotkeys")
-end
-
-keycodes.inputsourcechanged(rebindHotkeys)
-
-createHotkeys()
-alert.show("Mjolnir, at your service.")
+init()
